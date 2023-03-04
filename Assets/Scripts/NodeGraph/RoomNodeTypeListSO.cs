5bf46d37a9a340c0cabd10c3de3a6efa8ac4bb2d
Jeremy Hendra Jaya Raja Bakti <jeremyhjrb@gmail.com>
2023-03-04 23:35:41 +0700

    5.3_Room Node Graphs and Room Nodes Overview

---
+++ Assets/Scripts/NodeGraph/RoomNodeTypeListSO.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "RoomNodeTypeListSO", menuName = "Scriptable Objects/Dungeon/Room Node Type List")]
+
+public class RoomNodeTypeListSO : ScriptableObject
+{
+    public List<RoomNodeTypeSO> list;
+
+    #region Validation
+#if UNITY_EDITOR
+    private void OnValidate(){
+        HelperUtilities.ValidateCheckEnumerableValues(this, nameof(list), list);
+    }
+#endif
+    #endregion
+}
