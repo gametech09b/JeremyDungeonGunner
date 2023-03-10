11eda0923eb212075cfe2f2d668b6e37c55056f5
Jeremy Hendra Jaya Raja Bakti <jeremyhjrb@gmail.com>
2023-03-10 16:37:57 +0700

    5.12_Create Dungeon Layout Room Graphs

--- Assets/Scripts/NodeGraph/Editor/RoomNodeGraphEditor.cs
+++ Assets/Scripts/NodeGraph/Editor/RoomNodeGraphEditor.cs
@@ -269,7 +269,7 @@ public class RoomNodeGraphEditor : EditorWindow
             CreateRoomNode(new Vector2(200f, 200f), roomNodeTypeList.list.Find(x => x.isEntrance));
         }
 
-        CreateRoomNode(mousePositionObject, roomNodeTypeList.list.Find(x => x.isNone));
+        CreateRoomNode(mousePositionObject, roomNodeTypeList.list.Find(x => x.isCorridor));
     }
 
     /// <summary>
