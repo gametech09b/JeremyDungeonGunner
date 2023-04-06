e9938a2e7b580141c1589855fc7face32fea9ae9
Jeremy Hendra Jaya Raja Bakti <jeremyhjrb@gmail.com>
2023-04-06 22:03:28 +0700

    10.5_Player Dodge Roll

--- Assets/Scripts/Movement/MovementDetailsSO.cs
+++ Assets/Scripts/Movement/MovementDetailsSO.cs
@@ -17,6 +17,18 @@ public class MovementDetailsSO : ScriptableObject
     [Tooltip("The maximum move speed. The GetMoveSpeed method calculates a random value between the minimum and maximum")]
     #endregion Tooltip
     public float maxMoveSpeed = 8f;
+    #region Tooltip
+    [Tooltip("If there is a roll movement- this is the speed of the roll")]
+    #endregion
+    public float rollSpeed;
+    #region Tooltip
+    [Tooltip("If there is a roll movement- this is the distance of the roll")]
+    #endregion
+    public float rollDistance;
+    #region Tooltip
+    [Tooltip("If there is a roll movement- this is the cooldown time in seconds between the roll action")]
+    #endregion
+    public float rollCooldownTime;
 
     /// <summary>
     /// Get a random movement speed between the minimum and maximum values
@@ -40,6 +52,12 @@ public class MovementDetailsSO : ScriptableObject
     {
         HelperUtilities.ValidateCheckPositiveRange(this, nameof(minMoveSpeed), minMoveSpeed, nameof(maxMoveSpeed), maxMoveSpeed, false);
 
+        if (rollDistance != 0f || rollSpeed != 0 || rollCooldownTime != 0)
+        {
+            HelperUtilities.ValidateCheckPositiveValue(this, nameof(rollDistance), rollDistance, false);
+            HelperUtilities.ValidateCheckPositiveValue(this, nameof(rollSpeed), rollSpeed, false);
+            HelperUtilities.ValidateCheckPositiveValue(this, nameof(rollCooldownTime), rollCooldownTime, false);
+        }
     }
 
 #endif
--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -3,12 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Rendering;
 
-
 #region REQUIRE COMPONENTS
 [RequireComponent(typeof(Health))]
 [RequireComponent(typeof(PlayerControl))]
 [RequireComponent(typeof(MovementByVelocityEvent))]
 [RequireComponent(typeof(MovementByVelocity))]
+[RequireComponent(typeof(MovementToPositionEvent))]
+[RequireComponent(typeof(MovementToPosition))]
 [RequireComponent(typeof(IdleEvent))]
 [RequireComponent(typeof(Idle))]
 [RequireComponent(typeof(AimWeaponEvent))]
@@ -23,13 +24,12 @@ using UnityEngine.Rendering;
 [DisallowMultipleComponent]
 #endregion REQUIRE COMPONENTS
 
-
-
 public class Player : MonoBehaviour
 {
     [HideInInspector] public PlayerDetailsSO playerDetails;
     [HideInInspector] public Health health;
     [HideInInspector] public MovementByVelocityEvent movementByVelocityEvent;
+    [HideInInspector] public MovementToPositionEvent movementToPositionEvent;
     [HideInInspector] public IdleEvent idleEvent;
     [HideInInspector] public AimWeaponEvent aimWeaponEvent;
     [HideInInspector] public SpriteRenderer spriteRenderer;
@@ -40,6 +40,7 @@ public class Player : MonoBehaviour
         // Load components
         health = GetComponent<Health>();
         movementByVelocityEvent = GetComponent<MovementByVelocityEvent>();
+        movementToPositionEvent = GetComponent<MovementToPositionEvent>();
         idleEvent = GetComponent<IdleEvent>();
         aimWeaponEvent = GetComponent<AimWeaponEvent>();
         spriteRenderer = GetComponent<SpriteRenderer>();
